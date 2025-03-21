3c9819cae6c398fdf3db5fe24141241ec7d51bb0
Steve Trefethen <strefethen@users.noreply.github.com>
2025-03-21 15:58:32 -0700

    fix: update start script to use ts-node and improve error logging in index.ts

--- ts-client/src/index.ts
+++ ts-client/src/index.ts
@@ -5,23 +5,23 @@ const API: ItemsAPI.ItemsResource = new ItemsAPI.ItemsResource("http://localhost
 API.createItem({ name: "New Item", description: "Description", price: 100 }).then((item) => {
   console.log(item);
 }).catch(err => {
-  console.log(err.message);
+  console.error(err.message);
 });
 
 API.getItemsCount().then((count) => {
   console.log(count);
 }).catch(err => {
-  console.log(err.message);
+  console.error(err.message);
 });
 
 API.getItems().then((items) => {
   console.log(items);
 }).catch(err => {
-  console.log(err.message);
+  console.error(err.message);
 });
 
 API.deleteItemById(1).then((response) => {
   console.log(response);
 }).catch(err => {
-  console.log(err.message);
+  console.error(err.message);
 });
