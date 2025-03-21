d7aca1b7429fb7a2e3fd1dfc9c821ebd6dd691ec
Steve Trefethen <strefethen@users.noreply.github.com>
2025-03-21 12:41:38 -0700

    Updated python client instructions and code

---
+++ oas-client-python/src/main.py
@@ -0,0 +1,27 @@
+import openapi_client
+from openapi_client.rest import ApiException
+from pprint import pprint
+
+# Defining the host is optional and defaults to http://localhost:3000
+# See configuration.py for a list of all supported configuration parameters.
+configuration = openapi_client.Configuration(
+    host = "http://localhost:3000"
+)
+
+
+
+# Enter a context with an instance of the API client
+with openapi_client.ApiClient(configuration) as api_client:
+    # Create an instance of the API class
+    api_instance = openapi_client.ItemsApi(api_client)
+    item = {"name":"New Item","description":"Description of the new item","price":15.0} # Item | The details of the new item to create.
+
+    try:
+        # Create a new item
+        api_response = api_instance.create_item(item)
+        print("The response of ItemsApi->create_item:\n")
+        pprint(api_response)
+        api_response = api_instance.get_item_count()
+        pprint(api_response)
+    except ApiException as e:
+        print("Exception when calling ItemsApi->create_item: %s\n" % e)
