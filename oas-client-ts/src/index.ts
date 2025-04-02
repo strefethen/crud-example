51db87c0a04963e5c003f1e65db4d2f3ea209557
Steve Trefethen <strefethen@users.noreply.github.com>
2025-04-02 11:11:07 -0700

    Update typescript client to use auth

--- oas-client-ts/src/index.ts
+++ oas-client-ts/src/index.ts
@@ -1,18 +1,31 @@
-import * as API from '../generated';
+import * as API from '../client/generated';
 
-const configuration = new API.Configuration({
+const configuration: API.Configuration = {
   basePath: 'http://localhost:3000',
-});
+  isJsonMime: (mime) => {
+    return mime === 'application/json' || mime === 'application/vnd.api+json';
+  },
+  accessToken: ""  // First call must be to createSession to fetch the token.
+}
 
-const itemsAPI = new API.ItemsApi(configuration);
+const authAPI = new API.AuthenticationApi(configuration);
 
-async function main() {
+(async () => {
+  const session = await authAPI.createSession({ username: 'stevet' })
+  console.log(authAPI);
+  configuration.accessToken = session.data.token;
+  const itemsApi = new API.ItemsApi(
+    configuration
+  );
+  
+  const itemsAPI = new API.ItemsApi(configuration);
+  
   await itemsAPI.getItems().then((items) => {
     console.log(items.data);
   }).catch((error) => {
     console.error(error);
   });
-  
+    
   await itemsAPI.createItem({
     description: "test",
     name: "Item Name",
@@ -21,14 +34,12 @@ async function main() {
     console.error(error);
   });  
 
-  const count = await itemsAPI.getItemCount();
-  console.log(count);
-
-//  await itemsAPI.deleteItemById(count - 1).catch((error) => {
-//    console.error(error);
-//  });
-}
-
-main().catch(err => {
-  console.log(err);
-})
+  const getCount = await itemsAPI.getItemCount();
+  console.log(getCount.data.count);
+  
+  await itemsAPI.deleteItemById(getCount.data.count - 1).catch((error) => {
+    console.error(error);
+  });
+})().catch((err) => {
+  console.log('Error: ' + err);
+});
